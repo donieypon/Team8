c1ac282d55a3c48c7f4f61ee2ecba10702beba14
Donie Ypon <donieypon@donies-mbp.attlocal.net>
2019-10-27 23:50:35 -0700

    Implemented login and flask-bootstrap

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,59 @@
-from flask import Flask
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, flash, redirect, url_for
+from app import app
 from app import db
-from flask_login import current_user
-from flask_login import logout_user
-from app.models import User
+from app.forms import LoginForm
 from app.forms import createAccount
+from app.models import User
+from flask_login import current_user, login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'some-key'
+Bootstrap(app)
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        # look at first result first()
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        # return to page before user got asked to login
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
+    return render_template('login.html', title='Sign in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -22,12 +68,3 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
