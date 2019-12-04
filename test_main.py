5c0d7829514db81d43b5d5c8f4053bad4375e5ba
YenniLamb <47261678+YenniLamb@users.noreply.github.com>
2019-12-03 22:37:29 -0800

    pytest 1
    testing the validate new user email and password

--- test_main.py
+++ test_main.py
@@ -1,2 +1,16 @@
-def test():
-  pass
+import pytest
+from app import routes
+from app.models import User, Post
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_user, logout_user
+
+@pytest.fixture(scope = 'module')
+def new_user():
+  user = User(email = 'yennhilam@ymail.com' , username = 'username', password_hash = generated_password_hash('1234'))
+  return user
+
+# Pytest 1
+# testing validating new user yennhilam@ymail.com
+def test_new_user(new_user):
+  assert new_user.email == 'yennhilam@ymail.com'
+  assert new_user.password_hash != generate_password_hash('1234')
