10ea292679f217c7d5dae9291cea4e72adf8f8e7
thayash1 <54564699+thayash1@users.noreply.github.com>
2019-12-04 04:32:17 -0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,19 +6,25 @@ from flask_login import current_user, login_user, logout_user
 
 @pytest.fixture(scope = 'module')
 def new_user():
-  user = User(email = 'yennhilam@ymail.com' , username = 'username', password_hash = generated_password_hash('1234'))
+  user = User(email = 'yennhilam@ymail.com' , username = 'username', password_hash = '1234')
   return user
 
 # Pytest 1
 # testing validating new user yennhilam@ymail.com <---  maybe need to change
 def test_new_user(new_user):
   assert new_user.email == 'yennhilam@ymail.com'
-  assert new_user.password_hash != generate_password_hash('1234')
+  assert new_user.password_hash != '1234'
   
 # Pytest 2
 # verifying the new user is yennhilam@ymail.com
 def test_login(new_user):
   assert new_user.is_authenticated == True
+  
+# Pytest 3
+# Testing email is incorrect, but password is correct
+def test_new_user(new_user):
+  assert new_user.email != 'yennhilam@ymail.com'
+  assert new_user.password_hash == '1234'
   
   
 
