f9336fc3fe6a4b33bf1c4ee9e8ce8c0196f8076b
Donie Ypon <donieypon@donies-mbp.attlocal.net>
2019-12-09 00:05:06 -0800

    Added docstrings to methods/classes

--- app/forms.py
+++ app/forms.py
@@ -5,12 +5,18 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
+    '''
+    Class handles logging in for existing users. Variables are defined with WTForm fields.
+    '''
     username = StringField('Username', render_kw={"placeholder": "Enter Username"}, validators=[DataRequired()])
     password = PasswordField('Password', render_kw={"placeholder": "Password"}, validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class createAccount(FlaskForm):
+    '''
+    Class handles registering for new users. Variables are defined with WTForm fields.
+    '''
     username = StringField('Username', render_kw={"placeholder": "Enter Username"}, validators=[DataRequired()])
     email = StringField('Email', render_kw={"placeholder": "Enter email"}, validators=[DataRequired(), Email()])
     password = PasswordField('Password', render_kw={"placeholder": "Enter password"}, validators=[DataRequired()])
@@ -19,42 +25,63 @@ class createAccount(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        '''Validates username
+        :param username:
+        :return: Checks if username is not taken; if taken, prompts user to try again
+        '''
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
+        '''Validates email
+
+        :param email:
+        :return: Checks if email is not taken; if taken, prompts user to try again
+        '''
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a valid email address.')
 
 class PostForm(FlaskForm):
+    '''
+    Class handles the creation of new tasks.
+    '''
     nameTitle = StringField('nameTitle', validators=[DataRequired()])
     content = TextAreaField('content')
     complete = BooleanField('complete')
     submit = SubmitField('Create')
 
-class addFriend(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()], description="Enter friend's username")
-    message = StringField('Message', description="Send a message")
-    add = SubmitField('Add')
-
 class mailForm(FlaskForm):
+    '''
+    Class handles sending messages to other users; used by mes()
+    '''
     email = StringField('Email', validators=[DataRequired()])
     subject = StringField('Subject')
     content = TextAreaField('content', validators=[DataRequired()])
     submit = SubmitField('Send')
 
 class forgotForm(FlaskForm):
+    '''
+    Class handles request for reset of password; used by resetRequest()
+    '''
     email = StringField('Email', render_kw={"placeholder": "Enter Email"}, validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
+        '''Validates email
+
+        :param email:
+        :return: Checks if email is not in the system, prompts user to try again
+        '''
         user = User.query.filter_by(email=email.data).first()
         if user is None:
             raise ValidationError('There is no account with that email. You must register first.')
 
 class passwordResetForm(FlaskForm):
+    '''
+    Class handles the reset of password after user clicks on link.
+    '''
     password = PasswordField('Password', validators=[DataRequired()])
     confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
--- app/models.py
+++ app/models.py
@@ -4,12 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-followers = db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
-
 class User(UserMixin, db.Model):
+    '''
+    Class is responsible for each user's credentials as well as password resets.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -28,23 +26,6 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    # def follow(self, user):
-    #     if not self.is_following(user):
-    #         self.followed.append(user)
-    #         return self
-    #
-    # def unfollow(self, user):
-    #     if self.is_following(user):
-    #         self.followed.remove(user)
-    #         return self
-    #
-    # def is_following(self, user):
-    #     return self.followed.filter(followers.c.followed_id == user.id).count() > 0
-    #
-    # def followed_posts(self):
-    #     return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
-    #         followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
-
     def getResetToken(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
@@ -62,6 +43,9 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
 
 class Post(UserMixin,db.Model):
+    '''
+    Class is responsible for creating new tasks.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     nameTitle = db.Column(db.String(256),  index=True)
     content = db.Column(db.UnicodeText, index=True)
@@ -71,16 +55,6 @@ class Post(UserMixin,db.Model):
 
     def __repr__(self):
         return '<Posts {}>'.format(self.nameTitle)
-
-class Friend(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    friend_username = db.Column(db.String(64), index=True)
-    friend_email = db.Column(db.String(64), index=True)
-    friend_id = db.Column(db.Integer, index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Friend {}>'.format(self.id)
 
 @login.user_loader
 def load_user(id):
