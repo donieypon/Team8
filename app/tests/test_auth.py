9d453d7fbb4cfd2d52dad480613344fccd59316c
YenniLamb <47261678+YenniLamb@users.noreply.github.com>
2019-12-09 23:17:23 -0800

    8 working unit test

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -2,7 +2,7 @@ import pytest
 # from app import models, forms
 from app.models import User, Post
 from app.forms import LoginForm
-#from app import routes
+# from app import routes
 
 
 def test_login(client):
@@ -22,9 +22,40 @@ def test_verify_user_exists(db):
     
 def test_valid_register(client, db):
     response = client.post('/login',
-                           data=dict(username='user', email='testing@testing.com',
+                           data=dict(uasername='user', email='testing@testing.com',
                                      password='testing', confirm='testing'),
                            follow_redirects=True)
     assert response.status_code == 200  
 
-    
+
+#  testing the add note 
+def test_addNotes(db):
+    post = Post(nameTitle= 'Studying for all Finals',user_id='1', content='studying')
+    db.session.add(post)
+    db.session.commit()
+    assert len(User.query.all()) == 1
+    assert Post.query.get(1).content =='studying'
+    assert Post.query.get(1).nameTitle == 'Studying for all Finals'
+
+# testing login page is html 
+def test_index(client):
+     response = client.get('/login', content_type='html/text')
+     assert response.status_code == 200
+
+def test_user_requires_login(client):
+    response = client.get("/", follow_redirects=True)
+    assert b"Please log in to access this page." in response.data
+
+
+
+def test_finish(client,db):
+     response = client.post('/login',
+                           data=dict(uasername='huan89', password='huean89'),
+                           follow_redirects=True) 
+
+     response = client.post("/edit", data=dict(status=True), follow_redirects=True )
+     assert response.status_code == 404
+#                         
+
+
+
